bc07e9d0f7ed907128d1be574c3bf130f69d813e
sssseol <nothinonthere@icloud.com>
2024-01-01 20:18:47 +0000

    [update]

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+
+const headerNav=[
+    {
+        title:"intro",
+        url:"#intro"
+    },
+    {
+        title:"skill",
+        url:"#skill"
+    },
+    {
+        title:"site",
+        url:"#site"
+    },
+    {
+        title:"portfolio",
+        url:"#port"
+    },
+    {
+        title:"contact",
+        url:"#contact"
+    }
+]
+
+const Header = () => {
+    const[show, setShow]=useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
+    return(
+        <header id="header" role="heading">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">portfolio<em>next.js</em></a>
+                </div>
+                <nav className={`header__nav${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+                    <ul>
+                        {headerNav.map((nav, key) =>(
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className="header__nav__mobile" id="headerToggle" aria-controls="primary-menu" aria-expanded="false" role="button" tabIndex="0" onClick={toggleMenu}>
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
